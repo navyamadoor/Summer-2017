65e7f463bf107524a9cde31d93d4ab820db2b54f
Navya Madoor <navyamadoor@Navyas-MacBook-Pro.local>
2017-05-30 18:19:12 -0400

    Initial Commit

---
+++ Dhirana/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dhirana
+//
+//  Created by Navya Madoor on 5/30/17.
+//  Copyright © 2017 Navya Madoor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
